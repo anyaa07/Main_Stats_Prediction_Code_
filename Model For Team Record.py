7665d1578a6211e87f1e7e3a3ae09386e6fdbf66
anyaa07 <anyanagpal11@gmail.com>
2023-05-31 20:57:16 -0400

    ideas

---
+++ Model For Team Record.py
@@ -0,0 +1,8 @@
+"""
+        # fit model to player data, merge 20 and 21 for x, 21 and 22 for y
+        Fit linear regression model
+        Model should learn pattern between team record & stats for a QB
+        Testing set = team record for 1 season & stats for one season
+        Training set = team record for 2 seasons & stats for two seasons
+        Merge the team record file and QB stats file on Team column
+"""
